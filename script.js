042836d0daf14e8ca4013fc5d4e55433ae6763b5
brianthoeurn <brianmonirath@hotmail.com>
2022-06-26 08:31:38 -0700

    Forgot to commit yesterday

--- script.js
+++ script.js
@@ -37,3 +37,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
   console.log('hello')
 console.log('red')
+
+
+/*Testing nav*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
