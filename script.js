54d86fbb60573bc56ac711641517369c934417c0
brianthoeurn <brianmonirath@hotmail.com>
2022-06-27 00:36:31 -0700

    IM SO LOST

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-
-window.addEventListener('DOMContentLoaded', ()=>{
-
-
-
-
-    setTimeout(()=>{
-
-        logoSpan.forEach((span, index)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (index + 1) * 400)
-        });
-
-        setTimeout(()=>{
-            logoSpan.forEach((span, index)=>{
-                
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (index+ 1) * 50)
-            })
-        },2000);
-
-            setTimeout(()=>{
-                intro.style.top = '-100vh';
-            }, 2300)
-
-    })
-})
-
-
-
-
-  console.log('hello')
-console.log('red')
-
-
-/*Testing nav*/
+// SIDE NAVIGATION
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-function closeNav() {
+  function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+
+
+
+
+
+
+
+
+
+
+// LOGO TRANSITION
+const intro = document.querySelector('.intro');
+const logoBtn = document.querySelector('.welcomeLogo');
+const myName = document.querySelector('#myname')
+
+const overLay = function () {
+    logoBtn.classList.add('fade');
+    setTimeout(()=>{
+        intro.style.top = '-100vh';
+    },500)
+    setTimeout(()=>{
+      myName.classList.remove('hide')
+    }, 1100)
+}
+
+logoBtn.addEventListener('click', overLay)
